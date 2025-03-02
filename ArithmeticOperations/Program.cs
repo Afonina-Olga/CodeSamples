aa2ac44cf9189d0a0dcf8a3f46ebc2505ab1971b
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-03 00:06:52 +0300

    Арифметические операции

---
+++ ArithmeticOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿// Арифметические операции -,*,/,+
+
+#region Сумма +
+
+#endregion
+
+#region  Разность -
+
+#endregion
+
+#region  Произведение *
+
+#endregion
+
+#region Частное /
+
+#endregion
+
+#region Остаток от деления %
+
+#endregion
