bb80b4f856b6bf5f954df6cc45e5c05126354189
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-04 15:33:50 +0300

    Циклы, операции

--- Conversions/Program.cs
+++ Conversions/Program.cs
@@ -1,10 +1,11 @@
-﻿#region Тиды данных
+﻿// https://metanit.com/sharp/tutorial/2.2.php
+
+#region Тиды данных
 
 // byte от 0 до 255
 // ushort от 0 до 65535
 // uint от 0 до 2^32-1 >>> от 0 до 4 294 967 296
 // ulong от 0 до 2^64-1 >>> от 0 до 18 446 744 073 709 551 616
-
 
 // sbyte от -128 дo +127
 // short от -32768 дo +32767
@@ -17,10 +18,39 @@
 
 #endregion
 
+#region Сужающие и расширяющие преобразования
+
+// Преобразования могут быть сужающие (narrowing) и расширяющие (widening).
+// Расширяющие преобразования расширяют размер объекта в памяти. Например:
+
+byte a = 4;             // 0000100
+ushort b = a;   // 000000000000100
+
+// В данном случае переменной типа ushort присваивается значение типа byte.
+// Тип byte занимает 1 байт (8 бит), и значение переменной a в двоичном виде
+// можно представить как: 00000100
+
+// Значение типа ushort занимает 2 байта (16 бит). И при присвоении переменной
+// b значение переменной a расширяется до 2 байт: 0000000000000100
+
+// То есть значение, которое занимает 8 бит, расширяется до 16 бит.
+
+// Сужающие преобразования, наоборот, сужают значение до типа меньшей разядности. 
+// В этом случае нужно явное приведение типов
+
+ushort aa = 4;
+byte bb = (byte)aa;
+
+// Здесь переменной b, которая занимает 8 бит, присваивается значение ushort,
+// которое занимает 16 бит. То есть из 0000000000000100 получаем 00000100.
+// Таким образом, значение сужается с 16 бит (2 байт) до 8 бит (1 байт).
+
+#endregion
+
 #region Введение в преобразование типов данных
 
-byte b = 100;   // Неявные преобразования
-int a = b;      // Всё хорошо
+byte bbb = 100;   // Неявные преобразования
+int aaa = bbb;      // Всё хорошо
 
 //int i = 10;     //
 //byte j = i;     // Ошибка компилятора
@@ -33,6 +63,17 @@ double d = a;   // Всё хорошо
 
 #region Неявные преобразования
 
+/*
+Расширяющие преобразования от типа с меньшей разрядностью к типу с большей 
+разрядностью компилятор проводит неявно. Это могут быть следующие цепочки 
+преобразований:
+
+byte -> short -> int -> long -> decimal
+int -> double
+short -> float -> double
+char -> int 
+*/
+
 byte byteValue = 1;                 // Задание значения переменной типа byte
 short shortValue = byteValue;       // Неявное приведение byte к short
 int intValue = shortValue;          // Неявное приведение short к int
@@ -44,9 +85,30 @@ char c = 'я';
 int dd = c;
 Console.WriteLine(dd);
 
+byte one = 4;
+int two = one + 70; // byte + int => int
+					// Сохраняем сумму byte и int в переменной int => Все ОК
+
+// Но теперь попробуем применить сложение к двум объектам типа byte:
+//byte a = 4;
+//byte b = a + 70;  // ошибка
+
+//При операциях мы должны учитывать диапазон значений, которые может хранить тот
+//или иной тип. Но в данном случае число 74, которое мы ожидаем получить, вполне
+//укладывается в диапазон значений типа byte, тем не менее мы получаем ошибку.
+
+//Дело в том, что операция сложения (да и вычитания) возвращает значение типа int,
+//если в операции участвуют целочисленные типы данных с разрядностью меньше или
+//равно int (то есть типы byte, short, int). Поэтому результатом операции a + 70
+//будет объект, который имеет длину в памяти 4 байта. Затем этот объект мы пытаемся
+//присвоить переменной b, которая имеет тип byte и в памяти занимает 1 байт.
+
+// Для решения этой проблемы нужны явные преобразования типов
 #endregion
 
 #region Явные преобразования
+// Операция преобразования типов предполагает указание в скобках того типа,
+// к которому надо преобразовать значение
 
 //int i = 330;    //
 //byte j = i;     // Ошибка компилятора
@@ -74,9 +136,7 @@ int q = (int)((byte)d);
 
 //string inputString = "2029";
 //int intValue = (int)inputString;     // Ошибка CS0029  
-//                                // Не удается неявно преобразовать тип "string" в "int".
-
-
+									 //                                // Не удается неявно преобразовать тип "string" в "int".
 string inputString = "2029";
 int intValue1 = int.Parse(inputString);
 Console.WriteLine(intValue1);                // 2029
@@ -87,9 +147,9 @@ Console.WriteLine(doubleValue1);             // 2029
 ulong ulongValue = ulong.Parse(inputString);
 Console.WriteLine(ulongValue);              // 2029
 
-//byte byteValue1 = byte.Parse(inputString);
-//Console.WriteLine(byteValue1);              // Необработанное исключение: 
-//											// System.OverflowException: Значение было недопустимо 
+// byte byteValue1 = byte.Parse(inputString);
+// Console.WriteLine(byteValue1);              // Необработанное исключение: 
+											// System.OverflowException: Значение было недопустимо 
 											// малым или недопустимо большим для беззнакового байта.
 
 #endregion
@@ -97,7 +157,6 @@ Console.WriteLine(ulongValue);              // 2029
 #region ConvertTo
 
 // Более гибкие преобразования для любых типов данных
-
 string inputString1 = "2029";
 //int intValue = int.Parse(inputString1);
 int intValue2 = Convert.ToInt32(inputString1);
@@ -123,22 +182,22 @@ Console.WriteLine(ulongValue1);              // 2029
 
 #region Типы данных и их синонимы
 
-//Тип данных  Тип.NET
-//sbyte       System.SByte
-//short       System.Int16
-//int         System.Int32
-//long        System.Int64
-//byte        System.SByte
-//ushort      System.UInt16
-//uint        System.UInt32
-//ulong       System.UInt64
-//float       System.Single
-//double      System.Double
-//bool        System.Boolean
+// Тип данных  Тип.NET
+// sbyte       System.SByte
+// short       System.Int16
+// int         System.Int32
+// long        System.Int64
+// byte        System.SByte
+// ushort      System.UInt16
+// uint        System.UInt32
+// ulong       System.UInt64
+// float       System.Single
+// double      System.Double
+// bool        System.Boolean
 
 #endregion
 
-#region Замечание
+#region Checked, unchecked
 
 /*
 В C# допускается указывать, будет ли в коде сгенерировано исключение при 
@@ -162,3 +221,4 @@ unchecked
 }
 
 #endregion
+
---
+++ If/Program.cs
@@ -0,0 +1,233 @@
+﻿// Условные конструкции - один из базовых компонентов многих языков программирования,
+// которые направляют работу программы по одному из путей в зависимости от
+// определенных условий. 
+
+// Конструкция if/else проверяет истинность некоторого условия и в зависимости от
+// результатов проверки выполняет определенный код.
+
+// Ее простейшая форма состоит из блока if:
+
+//if (условие)
+//{
+//	выполняемые инструкции
+//}
+
+// После ключевого слова if ставится условие. Условие должно представлять
+// значение типа bool. Это может быть непосредственно значение типа bool
+// или результат условного выражения или другого выражения, которое возвращает
+// значение типа bool. И если это условие истинно (равно true), то срабатывает
+// код, который помещен далее после условия внутри фигурных скобок
+
+#region Только if
+
+if (2 == 2)
+{
+	Console.WriteLine("Равно");
+}
+
+if (2 * 2 == 4)
+{
+	Console.WriteLine("Дважды два четыре!");
+}
+
+// Если блок if содержит одну инструкцию, то мы можем его сократить,
+// убрав фигурные скобки (но лучше сразу привыкать везде расставлять скобки,
+// даже если кажется, что они не нужны)
+
+if (5 > 4) Console.WriteLine("True");
+
+// Также мы можем соединить сразу несколько условий, используя логические операторы:
+int num1 = 8;
+int num2 = 6;
+
+if (num1 > num2 && num1 == 8)
+{
+	Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+
+#endregion
+
+#region else
+
+// Но что, если мы захотим, чтобы при несоблюдении условия также выполнялись
+// какие-либо действия? В этом случае мы можем добавить блок else:
+
+int first = 8;
+int second = 6;
+
+if (first > second)
+{
+	Console.WriteLine($"Число {first} больше числа {second}");
+}
+else
+{
+	Console.WriteLine($"Число {first} меньше числа {second}");
+}
+
+// Блок else выполняется, если условие после if ложно, то есть равно false.
+
+// Можно сократить (но не нужно):
+
+// int num1 = 8;
+// int num2 = 6;
+// if (num1 > num2)
+//		Console.WriteLine($"Число {num1} больше числа {num2}");
+// else
+//		Console.WriteLine($"Число {num1} меньше числа {num2}");
+
+#endregion
+
+#region Тернарная операция
+
+// В дополнение к оператору if есть, так называемый, тернарный оператор (?:).
+// Этот оператор может заменить одно предложение if-else, которое 
+// присваивает значение конкретной переменной. 
+// Оператор принимает три выражения. 
+// Если первый оценивается как true, будет возвращено второе выражение, 
+// и если оно false, то возвращается третий
+
+// Синтаксис: [первый операнд - условие] ? [второй операнд] : [третий операнд]
+// Это всего лишь сокращенная запись if-else
+
+int x = 3;
+int y = 2;
+
+int z = x < y ? (x + y) : (x - y);
+Console.WriteLine(z);   // 1
+
+#endregion
+
+#region else if
+
+// Используя конструкцию else if, мы можем обрабатывать дополнительные условия:
+
+int n1 = 8;
+int n2 = 6;
+if (n1 > n2)
+{
+	Console.WriteLine($"Число {n1} больше числа {n2}");
+}
+else if (n1 < n2)
+{
+	Console.WriteLine($"Число {n1} меньше числа {n2}");
+}
+else
+{
+	Console.WriteLine("Число n1 равно числу n2");
+}
+
+// При необходимости можно добавить несколько выражений else if
+
+// По введённому номеру дня недели вывести на экран название этого дня или
+// сообщит, что введённое число не корректно
+
+Console.Write("Введите номер дня: ");
+int dayIndex = int.Parse(Console.ReadLine());
+
+#region Вариант 1
+
+if (dayIndex == 1)
+{
+	Console.WriteLine("Понедельник");
+}
+else
+{
+	if (dayIndex == 2)
+	{
+		Console.WriteLine("Вторник");
+	}
+	else
+	{
+		if (dayIndex == 3)
+		{
+			Console.WriteLine("Среда");
+		}
+		else
+		{
+			if (dayIndex == 4)
+			{
+				Console.WriteLine("Четверг");
+			}
+			else
+			{
+				if (dayIndex == 5)
+				{
+					Console.WriteLine("Пятница");
+				}
+				else
+				{
+					if (dayIndex == 6)
+					{
+						Console.WriteLine("Суббота");
+					}
+					else
+					{
+						if (dayIndex == 7)
+						{
+							Console.WriteLine("Воскресенье");
+						}
+						else
+						{
+							Console.WriteLine("На планете Земля в неделе семь дней");
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+#endregion
+
+#region Вариант 2
+
+if (dayIndex == 1) Console.WriteLine("Понедельник");
+else if (dayIndex == 2) Console.WriteLine("Вторник");
+else if (dayIndex == 3) Console.WriteLine("Среда");
+else if (dayIndex == 4) Console.WriteLine("Четверг");
+else if (dayIndex == 5) Console.WriteLine("Пятница");
+else if (dayIndex == 6) Console.WriteLine("Суббота");
+else if (dayIndex == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("На планете Земля в неделе семь дней");
+
+#endregion
+
+// Далее мы узнаем, как эту запись сократить
+// На практике так делать не нужно
+// Показано для примера if и много else
+
+#endregion
+
+#region switch
+// Замена if-else
+
+switch (dayIndex)
+{
+	case 1:
+		Console.WriteLine("Понедельник");
+		break;
+	case 2:
+		Console.WriteLine("Вторник");
+		break;
+	case 3:
+		Console.WriteLine("Среда");
+		break;
+	case 4:
+		Console.WriteLine("Четверг");
+		break;
+	case 5:
+		Console.WriteLine("Пятница");
+		break;
+	case 6:
+		Console.WriteLine("Суббота");
+		break;
+	case 7:
+		Console.WriteLine("Воскресенье");
+		break;
+	default:
+		Console.WriteLine("На планете Земля в неделе семь дней");
+		break;
+}
+
+#endregion
